ed66ef992f1ed40381714db1428708822509cd58
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2020-01-31 22:36:39 +0700

    a

--- app/src/main/java/android/example/com/notification/Activity/LoginActivity.java
+++ app/src/main/java/android/example/com/notification/Activity/LoginActivity.java
@@ -42,17 +42,17 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
         //i
-//        smalltobig = AnimationUtils.loadAnimation(this, R.anim.smalltobig);
-//        btta = AnimationUtils.loadAnimation(this, R.anim.btta);
-//        btta2 = AnimationUtils.loadAnimation(this, R.anim.btta2);
-//        logo = findViewById(R.id.logo);
-//        Appname = findViewById(R.id.Appname);
+        smalltobig = AnimationUtils.loadAnimation(this, R.anim.smalltobig);
+        btta = AnimationUtils.loadAnimation(this, R.anim.btta);
+        btta2 = AnimationUtils.loadAnimation(this, R.anim.btta2);
+        logo = findViewById(R.id.logo_sd);
+        Appname = findViewById(R.id.nama_sd);
 //        subAppname = findViewById(R.id.subName);
-//        btnlogin = findViewById(R.id.button);
-//        ET_nisn = findViewById(R.id.editText);
-//        ET_password = findViewById(R.id.editText2);
+        btnlogin = findViewById(R.id.btn_login);
+        ET_nisn = findViewById(R.id.et_nisn);
+        ET_password = findViewById(R.id.et_password);
 //
-//        daftar=findViewById(R.id.daftar);
+//        daftar=findViewById(R.id.txt_daftar);
 //        daftar.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -62,86 +62,86 @@ public class LoginActivity extends AppCompatActivity {
 //        });
 //
 //        //pass anim
-//        logo.startAnimation(smalltobig);
-//        Appname.startAnimation(btta);
+        logo.startAnimation(smalltobig);
+        Appname.startAnimation(btta);
 //        subAppname.startAnimation(btta);
-//        btnlogin.startAnimation(btta2);
-//        ET_nisn.startAnimation(btta2);
-//        ET_password.startAnimation(btta2);
+        btnlogin.startAnimation(btta2);
+        ET_nisn.startAnimation(btta2);
+        ET_password.startAnimation(btta2);
 //        //i
-//        mApiInterface = UtilsApi.getAPIService();
-//        sharedPreferences = LoginActivity.this.getSharedPreferences("remember", Context.MODE_PRIVATE);
-//
-//        btnlogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//            String nisn = ET_nisn.getText().toString();
-//            String password = ET_password.getText().toString();
-//            //required
-//            if(nisn.equals("")) {
-//                Toast.makeText(LoginActivity.this, "NISN Harus Diisi", Toast.LENGTH_SHORT).show();
-//            }else  if(password.equals("")) {
-//                Toast.makeText(LoginActivity.this, "Password Harus Diisi", Toast.LENGTH_SHORT).show();
-//            } else {
-//                //dialog
-//                progressDialog =new ProgressDialog(LoginActivity.this);
-//                progressDialog.setMessage("Mohon Tunggu...");
-//                progressDialog.setCancelable(false);
-//                progressDialog.show();
-//                //login
-//                retrofit2.Call<Siswa> postLogin= mApiInterface.postLogin(nisn,password);
-//                postLogin.enqueue(new Callback<Siswa>() {
-//                    @Override
-//                    public void onResponse(Call<Siswa> call, Response<Siswa> response) {
-//                        progressDialog.dismiss();
-//                        String id_device = response.body().getId_device();
-//                        String nisn = response.body().getNisn();
-//                        String token = response.body().getToken();
-//                        String nama_siswa = response.body().getNama_siswa();
-//                      // String keterangan = response.body().getKeterangan();
-//                       // String tanggal_absensi = response.body().getTanggal_absensi();
-//                        //String waktu_mulai = response.body().getWaktu_mulai();
-//                        //String waktu_selesai = response.body().getWaktu_selesai();
-//                        //String nama_pelajaran = response.body().getNama_pelajaran();
-//                        //String nama_rombel = response.body().getNama_rombel();
-//                        //String nama_gtk = response.body().getNama_gtk();
-//
-//                        Log.e("Berhasil Login", "Berhasil " +nisn+nama_siswa);
-//                        if(TextUtils.isEmpty(id_device)){
-//                            Toast.makeText(LoginActivity.this,"NISN atau Password salah", Toast.LENGTH_LONG).show();
-//                        } else{
-//                            Toast.makeText(LoginActivity.this,"Berhasil Login", Toast.LENGTH_LONG).show();
-//                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-//                            SharedPreferences.Editor editor =sharedPreferences.edit();
-//                            editor.putString("id_device", id_device);
-//                            editor.putString("nisn", nisn);
-//                            editor.putString("token", token);
-//                            editor.putString("nama_siswa", nama_siswa);
-//                            //editor.putString("keterangan", keterangan);
-//                            //editor.putString("tanggal_absensi", tanggal_absensi);
-//                            //editor.putString("waktu_mulai", waktu_mulai);
-//                            //editor.putString("waktu_selesai", waktu_selesai);
-//                            //editor.putString("nama_pelajaran", nama_pelajaran);
-//                            //editor.putString("nama_rombel", nama_rombel);
-//                            //editor.putString("nama_gtk", nama_gtk);
-//                            editor.apply();
-//                            startActivity(intent);
-//                        }
-//
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Call<Siswa> call, Throwable t) {
-//                        progressDialog.dismiss();
-//                        Log.e("gagal", "gagal" + t);
-//                        Toast.makeText(LoginActivity.this, "Gagal Login", Toast.LENGTH_LONG).show();
-//                    }
-//                });
-//             //batas else required
-//            }
-//            }
-//        //batas klik login
-//        });
+        mApiInterface = UtilsApi.getAPIService();
+        sharedPreferences = LoginActivity.this.getSharedPreferences("remember", Context.MODE_PRIVATE);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String nisn = ET_nisn.getText().toString();
+            String password = ET_password.getText().toString();
+            //required
+            if(nisn.equals("")) {
+                Toast.makeText(LoginActivity.this, "NISN Harus Diisi", Toast.LENGTH_SHORT).show();
+            }else  if(password.equals("")) {
+                Toast.makeText(LoginActivity.this, "Password Harus Diisi", Toast.LENGTH_SHORT).show();
+            } else {
+                //dialog
+                progressDialog =new ProgressDialog(LoginActivity.this);
+                progressDialog.setMessage("Mohon Tunggu...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                //login
+                retrofit2.Call<Siswa> postLogin= mApiInterface.postLogin(nisn,password);
+                postLogin.enqueue(new Callback<Siswa>() {
+                    @Override
+                    public void onResponse(Call<Siswa> call, Response<Siswa> response) {
+                        progressDialog.dismiss();
+                        String id_device = response.body().getId_device();
+                        String nisn = response.body().getNisn();
+                        String token = response.body().getToken();
+                        String nama_siswa = response.body().getNama_siswa();
+                      // String keterangan = response.body().getKeterangan();
+                       // String tanggal_absensi = response.body().getTanggal_absensi();
+                        //String waktu_mulai = response.body().getWaktu_mulai();
+                        //String waktu_selesai = response.body().getWaktu_selesai();
+                        //String nama_pelajaran = response.body().getNama_pelajaran();
+                        //String nama_rombel = response.body().getNama_rombel();
+                        //String nama_gtk = response.body().getNama_gtk();
+
+                        Log.e("Berhasil Login", "Berhasil " +nisn+nama_siswa);
+                        if(TextUtils.isEmpty(id_device)){
+                            Toast.makeText(LoginActivity.this,"NISN atau Password salah", Toast.LENGTH_LONG).show();
+                        } else{
+                            Toast.makeText(LoginActivity.this,"Berhasil Login", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            SharedPreferences.Editor editor =sharedPreferences.edit();
+                            editor.putString("id_device", id_device);
+                            editor.putString("nisn", nisn);
+                            editor.putString("token", token);
+                            editor.putString("nama_siswa", nama_siswa);
+                            //editor.putString("keterangan", keterangan);
+                            //editor.putString("tanggal_absensi", tanggal_absensi);
+                            //editor.putString("waktu_mulai", waktu_mulai);
+                            //editor.putString("waktu_selesai", waktu_selesai);
+                            //editor.putString("nama_pelajaran", nama_pelajaran);
+                            //editor.putString("nama_rombel", nama_rombel);
+                            //editor.putString("nama_gtk", nama_gtk);
+                            editor.apply();
+                            startActivity(intent);
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Siswa> call, Throwable t) {
+                        progressDialog.dismiss();
+                        Log.e("gagal", "gagal" + t);
+                        Toast.makeText(LoginActivity.this, "Gagal Login", Toast.LENGTH_LONG).show();
+                    }
+                });
+             //batas else required
+            }
+            }
+        //batas klik login
+        });
 
     }
 
