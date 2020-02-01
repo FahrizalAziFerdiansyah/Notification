dbd041325b61aac75d654d7e69ab64f19e1a10d7
ajipratama11 <47959982+ajipratama11@users.noreply.github.com>
2020-02-01 18:18:42 +0700

    a

--- app/src/main/java/android/example/com/notification/Activity/HomeActivity.java
+++ app/src/main/java/android/example/com/notification/Activity/HomeActivity.java
@@ -4,60 +4,159 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.animation.ValueAnimator;
+import android.example.com.notification.CurvedBottomNavigationView;
 import android.example.com.notification.Fragment.HomeFragment;
 import android.example.com.notification.Fragment.NotifFragment;
 import android.example.com.notification.Fragment.ProfileFragment;
 import android.example.com.notification.R;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.sdsmdg.harjot.vectormaster.VectorMasterView;
+import com.sdsmdg.harjot.vectormaster.models.PathModel;
 
-public class HomeActivity extends AppCompatActivity {
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment fragment;
-
-            switch (item.getItemId()) {
-                case R.id.home:
-
-                    fragment = new HomeFragment();
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
-                            .commit();
-                    return true;
-                case R.id.notification:
-
-                    fragment = new NotifFragment();
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
-                            .commit();
-                    return true;
-                case R.id.profile:
-
-                    fragment = new ProfileFragment();
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
-                            .commit();
-                    return true;
-            }
-            return false;
-        }
-    };
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    CurvedBottomNavigationView bottomNavigationView;
+    VectorMasterView fab, fab1, fab2;
+    RelativeLayout lin_id;
+    PathModel outline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //view
+        bottomNavigationView =findViewById(R.id.bottom_nav);
+        fab = findViewById(R.id.fab);
+        fab1 = findViewById(R.id.fab1);
+        fab2 = findViewById(R.id.fab2);
 
-        if (savedInstanceState==null){
-            navigation.setSelectedItemId(R.id.home);
+        lin_id = findViewById(R.id.lin_id);
+        bottomNavigationView.inflateMenu(R.menu.main_menu);
+
+        //set event
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment;
+
+        switch (menuItem.getItemId())
+        {
+            case R.id.home:
+//            Toast.makeText(this,"Click home", Toast.LENGTH_SHORT).show();
+                //anim
+                draw(6);
+                //find
+                lin_id.setX(bottomNavigationView.mFirstCurveControlPoint1.x);
+                fab.setVisibility(View.VISIBLE);
+                fab1.setVisibility(View.GONE);
+                fab2.setVisibility(View.GONE);
+                drawAnimation(fab);
+                fragment = new HomeFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                        .commit();
+                break;
+            case R.id.notif:
+//                Toast.makeText(this,"Click notif", Toast.LENGTH_SHORT).show();
+                //anim
+                draw(2);
+                //find
+                lin_id.setX(bottomNavigationView.mFirstCurveControlPoint1.x);
+                fab.setVisibility(View.GONE);
+                fab1.setVisibility(View.VISIBLE);
+                fab2.setVisibility(View.GONE);
+                drawAnimation(fab1);
+                fragment = new NotifFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                        .commit();
+                break;
+            case R.id.profile:
+//                Toast.makeText(this,"Click notif", Toast.LENGTH_SHORT).show();
+                //anim
+                draw();
+                //find
+                lin_id.setX(bottomNavigationView.mFirstCurveControlPoint1.x);
+                fab.setVisibility(View.GONE);
+                fab1.setVisibility(View.GONE);
+                fab2.setVisibility(View.VISIBLE);
+                drawAnimation(fab2);
+                fragment = new ProfileFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
+                        .commit();
+                break;
+
+
+
         }
+
+        return true;
+    }
+
+    private void draw() {
+        bottomNavigationView.mFirstCurveStartPoint.set((bottomNavigationView.mNavigationBarWidth *10/12) - (bottomNavigationView.CURVE_CIRCLE_RADIUS*2) - (bottomNavigationView.CURVE_CIRCLE_RADIUS/6), 0);
+
+        bottomNavigationView.mFirstCurveEndPoint.set(bottomNavigationView.mNavigationBarWidth*10/12,bottomNavigationView.CURVE_CIRCLE_RADIUS+(bottomNavigationView.CURVE_CIRCLE_RADIUS/4));
+
+        bottomNavigationView.mSecondCurveStartPoint = bottomNavigationView.mFirstCurveEndPoint;
+        bottomNavigationView.mSecondCurveEndPoint.set((bottomNavigationView.mNavigationBarWidth*10/12)+(bottomNavigationView.CURVE_CIRCLE_RADIUS*2)+(bottomNavigationView.CURVE_CIRCLE_RADIUS/3),0);
+
+        bottomNavigationView.mFirstCurveControlPoint1.set(bottomNavigationView.mFirstCurveStartPoint.x + bottomNavigationView.CURVE_CIRCLE_RADIUS + (bottomNavigationView.CURVE_CIRCLE_RADIUS/4), bottomNavigationView.mFirstCurveStartPoint.y);
+
+        bottomNavigationView.mFirstCurveControlPoint2.set(bottomNavigationView.mFirstCurveEndPoint.x - (bottomNavigationView.CURVE_CIRCLE_RADIUS*2)+bottomNavigationView.CURVE_CIRCLE_RADIUS, bottomNavigationView.mFirstCurveEndPoint.y);
+
+        //same
+        bottomNavigationView.mSecondCurveControlPoint1.set(bottomNavigationView.mSecondCurveStartPoint.x +(bottomNavigationView.CURVE_CIRCLE_RADIUS*2) - bottomNavigationView.CURVE_CIRCLE_RADIUS, bottomNavigationView.mSecondCurveStartPoint.y);
+
+        bottomNavigationView.mSecondCurveControlPoint2.set(bottomNavigationView.mSecondCurveEndPoint.x - (bottomNavigationView.CURVE_CIRCLE_RADIUS + (bottomNavigationView.CURVE_CIRCLE_RADIUS/4)),bottomNavigationView.mSecondCurveEndPoint.y);
+    }
+
+    private void drawAnimation(final VectorMasterView fab) {
+        outline = fab.getPathModelByName("outline");
+        outline.setStrokeColor(Color.WHITE);
+        outline.setTrimPathEnd(0.0f);
+        //init value
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        valueAnimator.setDuration(500);
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                outline.setTrimPathEnd((Float)valueAnimator.getAnimatedValue());
+                fab.update();
+            }
+        });
+        valueAnimator.start();
+    }
+
+    private void draw(int i)
+    {
+        bottomNavigationView.mFirstCurveStartPoint.set((bottomNavigationView.mNavigationBarWidth/i)-(bottomNavigationView.CURVE_CIRCLE_RADIUS*2)-(bottomNavigationView.CURVE_CIRCLE_RADIUS/6),0);
+
+        bottomNavigationView.mFirstCurveEndPoint.set(bottomNavigationView.mNavigationBarWidth/i,bottomNavigationView.CURVE_CIRCLE_RADIUS+(bottomNavigationView.CURVE_CIRCLE_RADIUS/4));
+
+        bottomNavigationView.mSecondCurveStartPoint = bottomNavigationView.mFirstCurveEndPoint;
+        bottomNavigationView.mSecondCurveEndPoint.set((bottomNavigationView.mNavigationBarWidth/i)+(bottomNavigationView.CURVE_CIRCLE_RADIUS*2)+(bottomNavigationView.CURVE_CIRCLE_RADIUS/3),0);
+
+        bottomNavigationView.mFirstCurveControlPoint1.set(bottomNavigationView.mFirstCurveStartPoint.x + bottomNavigationView.CURVE_CIRCLE_RADIUS + (bottomNavigationView.CURVE_CIRCLE_RADIUS/4), bottomNavigationView.mFirstCurveStartPoint.y);
+
+        bottomNavigationView.mFirstCurveControlPoint2.set(bottomNavigationView.mFirstCurveEndPoint.x - (bottomNavigationView.CURVE_CIRCLE_RADIUS*2)+bottomNavigationView.CURVE_CIRCLE_RADIUS, bottomNavigationView.mFirstCurveEndPoint.y);
+
+        //same
+        bottomNavigationView.mSecondCurveControlPoint1.set(bottomNavigationView.mSecondCurveStartPoint.x +(bottomNavigationView.CURVE_CIRCLE_RADIUS*2) - bottomNavigationView.CURVE_CIRCLE_RADIUS, bottomNavigationView.mSecondCurveStartPoint.y);
+
+        bottomNavigationView.mSecondCurveControlPoint2.set(bottomNavigationView.mSecondCurveEndPoint.x - (bottomNavigationView.CURVE_CIRCLE_RADIUS + (bottomNavigationView.CURVE_CIRCLE_RADIUS/4)),bottomNavigationView.mSecondCurveEndPoint.y);
+
     }
 }
